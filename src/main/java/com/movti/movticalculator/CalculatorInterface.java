f3a0566cb74dd5f46742dde29769f4ae636d0353
hp <momti@mail.ru>
2023-03-16 01:26:39 +0300

    Сделано домашнее задание

---
+++ src/main/java/com/movti/movticalculator/CalculatorInterface.java
@@ -0,0 +1,18 @@
+package com.movti.movticalculator;
+
+public interface CalculatorInterface {
+    public String welcomeToCalculator();
+
+
+
+    public int sum(int num1, int num2);
+
+
+    public int minus(int num1, int num2);
+    public int multiply(int num1, int num2);
+    public int divide(int num1, int num2);
+
+
+}
+
+
