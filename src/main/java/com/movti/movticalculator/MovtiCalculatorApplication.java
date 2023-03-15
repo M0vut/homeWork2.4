bad2162411ead00b3dc2a9dfa1f1912e9d3e6bd8
hp <momti@mail.ru>
2023-03-15 08:28:09 +0300

    первый коммит

---
+++ src/main/java/com/movti/movticalculator/MovtiCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.movti.movticalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovtiCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovtiCalculatorApplication.class, args);
+	}
+
+}
